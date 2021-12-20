a90d4aaef98941e099c89eae60f650a194bee2ec
JP <jp@jpcaparas.com>
2021-12-20 20:08:37 +1300

    Scaffold components for Vue UI

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+
+                    <p>
+                        <button @click="onClick">Click me to trigger an error.</button>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+
+        methods: {
+            onClick() {
+                throw new Error('Such is life.');
+            }
+        }
+    }
+</script>
---
+++ resources/views/debug-sentry-js.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Debug Sentry JS</title>
+    <link rel="stylesheet" type="text/css" src="{{ mix('/css/app.css') }}" />
+</head>
+
+<body>
+    <div id="app">
+        <example-component></example-component>
+    </div>
+    <script src="{{ mix('/js/app.js') }}"></script>
+</body>
+
+</html>
